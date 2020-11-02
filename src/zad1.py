fdb7408aee3d6b6749e3ffc5bc8f190f21b243e2
Kamil Sztabiński <tefay1@gmail.com>
2020-11-02 23:33:13 +0100

    refactor/zad1/test1-to-test5

--- src/zad1.py
+++ src/zad1.py
@@ -1,12 +1,6 @@
 class Hamming:
     def distance(self, string1, string2):
-        if string1 == "" and string2 == "":
-            return 0
-        elif string1 == "A" and string2 == "A":
-            return 0
-        elif string1 == "G" and string2 == "T":
-            return 1
-        elif len(string1) == len(string2):
+        if len(string1) == len(string2):
             if string1 == string2:
                 return 0
             else:
