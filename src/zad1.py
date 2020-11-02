804b42b75b4bd825711dae778f3618845bef8237
Kamil Sztabiński <tefay1@gmail.com>
2020-11-02 23:59:18 +0100

    refactor/zad1

--- src/zad1.py
+++ src/zad1.py
@@ -9,11 +9,5 @@ class Hamming:
                     if string1[i] != string2[i]:
                         counter += 1
                 return counter
-        elif len(string1) > len(string2) >= 1:
-            raise ValueError("str1 is longer than str2")
-        elif len(string2) > len(string1) >= 1:
-            raise ValueError("str2 is longer than str1")
-        elif len(string1) == 0 and len(string2) >= 1:
-            raise ValueError("str1 cannot be empty")
-        elif len(string1) >= 1 and len(string2) == 0:
-            raise ValueError("str2 cannot be empty")
+        else:
+            raise ValueError("strings should be the same length")
