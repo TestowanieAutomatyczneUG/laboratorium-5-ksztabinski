5beb4aa2b5742566d1e6ad98bd153b6eba8298ca
Kamil Sztabiński <tefay1@gmail.com>
2020-11-02 23:23:31 +0100

    green/zad1/test4

--- src/zad1.py
+++ src/zad1.py
@@ -6,3 +6,6 @@ class Hamming:
             return 0
         elif string1 == "G" and string2 == "T":
             return 1
+        elif len(string1) == len(string2):
+            if string1 == string2:
+                return 0
