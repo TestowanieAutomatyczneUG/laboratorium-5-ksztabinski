deb6e44b71c7968376723a69daca0ca85040e263
Kamil Sztabiński <tefay1@gmail.com>
2020-11-03 01:09:21 +0100

    green/zad2/test17

--- src/zad2.py
+++ src/zad2.py
@@ -11,7 +11,8 @@ class Roman:
             90: "XC",
             100: "C",
             400: "CD",
-            500: "D"
+            500: "D",
+            900: "CM"
         }
 
     def roman(self, value):
