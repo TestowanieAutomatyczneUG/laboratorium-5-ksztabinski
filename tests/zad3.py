ad76a1895d95f98ee284ef379d2ed8e790fb2c35
Kamil Sztabiński <tefay1@gmail.com>
2020-11-03 21:43:06 +0100

    red/zad3/test8

--- tests/zad3.py
+++ tests/zad3.py
@@ -38,7 +38,6 @@ class SongTest(unittest.TestCase):
     def test_get_sixth_verse(self):
         self.assertEqual(self.song.getVerse(6), 'On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
 
-    @unittest.skip
     def test_get_seventh_verse(self):
         self.assertEqual(self.song.getVerse(7), 'On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
 
