4715f810345a75bd7c4623fa4fb2a4e74b4fcd55
Kamil Sztabiński <tefay1@gmail.com>
2020-11-03 21:59:16 +0100

    green/zad3/test16

--- src/zad3.py
+++ src/zad3.py
@@ -26,3 +26,5 @@ class Song:
             return [self.lyrics.split('.')[i] + '.\n' for i in range(len(self.lyrics.split('.'))) if 0 <= i <= 2]
         elif num1 == 4 and num2 == 8:
             return [self.lyrics.split('.')[i] + '.\n' for i in range(len(self.lyrics.split('.'))) if 3 <= i <= 7]
+        elif num1 == 1 and num2 == 2:
+            return [self.lyrics.split('.')[i] + '.\n' for i in range(len(self.lyrics.split('.'))) if 0 <= i <= 1]
