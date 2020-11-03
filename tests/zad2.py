f0c124ef12c6f550aabc6f39850bbcff2f36ca9a
Kamil Sztabiński <tefay1@gmail.com>
2020-11-03 01:11:09 +0100

    green/zad2/test19

--- tests/zad2.py
+++ tests/zad2.py
@@ -56,7 +56,6 @@ class RomanNumeralsTest(unittest.TestCase):
     def test_1000_is_a_single_m(self):
         self.assertEqual(self.temp.roman(1024), "MXXIV")
 
-    @unittest.skip
     def test_3000_is_three_m_s(self):
         self.assertEqual(self.temp.roman(3000), "MMM")
 
