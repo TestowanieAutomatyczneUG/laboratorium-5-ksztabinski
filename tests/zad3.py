993fb946cb2597a1b06159278c203fd1a518e779
Kamil Sztabiński <tefay1@gmail.com>
2020-11-03 21:58:33 +0100

    red/zad3/test16

--- tests/zad3.py
+++ tests/zad3.py
@@ -72,7 +72,6 @@ class SongTest(unittest.TestCase):
             'On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n'
         ])
 
-    @unittest.skip
     def test_get_verses_from_1_to_2(self):
         self.assertEqual(self.song.getVerses(1, 2), [
             'On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.\n',
