55921451154959a49a9acd1416954439111f869b
Kamil Sztabiński <tefay1@gmail.com>
2020-11-03 21:47:52 +0100

    red/zad3/test13

--- tests/zad3.py
+++ tests/zad3.py
@@ -53,7 +53,6 @@ class SongTest(unittest.TestCase):
     def test_get_eleventh_verse(self):
         self.assertEqual(self.song.getVerse(11), 'On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
 
-    @unittest.skip
     def test_get_twelfth_verse(self):
         self.assertEqual(self.song.getVerse(12), 'On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.')
 
